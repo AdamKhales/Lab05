dc98089709604e37408994d3470063c1584719ad
Adam Khales <2493077@edu.vaniercollege.qc.ca>
2025-10-06 09:09:38 -0400

    Add visual elements (no eh)

--- src/lab05/Lab05.java
+++ src/lab05/Lab05.java
@@ -4,17 +4,85 @@
  */
 package lab05;
 
+import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 2493077
  */
-public class Lab05 {
+public class Lab05 extends Application{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(args);
     }
+
+    @Override
+    public void start(Stage stage) {
+        GridPane gp = new GridPane();
+        Label quantity = new Label("Select Quantity:");
+        Label bagStyle = new Label("Select Bag Style:");
+        ListView<String> bagList = new ListView<>();
+        bagList.setPrefWidth(250);
+        bagList.setPrefHeight(200);
+        bagList.getItems().addAll(
+                "Full Decorative",
+                "Beaded",
+                "Pirate Design",
+                "Fringed",
+                "Leather",
+                "Plain");
+        
+        ComboBox<Integer> cb = new ComboBox<>();
+        cb.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
+        cb.setValue(1);
+        
+        RadioButton small = new RadioButton("Small");
+        RadioButton medium = new RadioButton("Medium");
+        RadioButton large = new RadioButton("Large");
+        
+        ToggleGroup toggleGroup = new ToggleGroup();
+        small.setToggleGroup(toggleGroup);
+        medium.setToggleGroup(toggleGroup);
+        large.setToggleGroup(toggleGroup);
+        
+        Button order = new Button("Order");
+        order.setPrefWidth(100);
+        Button clear = new Button("Clear");
+        gp.setPadding(new Insets(10,10,10,10));
+        gp.setHgap(10);
+        gp.setHgap(10);
+        gp.add(bagStyle, 0, 0);
+        gp.add(bagList, 1, 0);
+        gp.add(quantity, 2, 0);
+        gp.add(cb, 3, 0);
+        gp.add(order, 2, 1);
+        gp.add(clear, 3, 1);
+        VBox radioBox = new VBox(10,small,medium,large);
+        radioBox.setAlignment(Pos.CENTER_LEFT);
+        gp.add(radioBox, 4, 0);
+        
+        
+        Scene scene = new Scene(gp, 800,300);
+        stage.setScene(scene);
+        stage.show();
+   }
     
 }
